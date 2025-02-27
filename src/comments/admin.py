70cb4cd571a1a276feead76297224ade88fff3e8
div2113 <divyajadhav2113@gmail.com>
2025-02-27 21:13:01 +0530

    blog_owner added

--- src/comments/admin.py
+++ src/comments/admin.py
@@ -3,8 +3,13 @@ from .models import Comment
 # Register your models here.
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display=["user","blog","content","created_at"]
+    list_display=["user","blog","content","created_at","blog_owner"]
     list_filter=["created_at"]
     search_fields=["user__username","blog__title","content"]
+
+    def blog_owner(self,obj):
+        return obj.blog.user.username
+    blog_owner.admin_order_field='blog_user'  #allows sorting
+    blog_owner.short_description='Blog Owner'   # Renames the column header
 
 admin.site.register(Comment,CommentAdmin)
