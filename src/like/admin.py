7c8c9a4fc37760655c37ea5fc30b77a66c49ec75
div2113 <divyajadhav2113@gmail.com>
2025-03-04 17:42:50 +0530

    likes works with admin panel

--- src/like/admin.py
+++ src/like/admin.py
@@ -7,5 +7,15 @@ class LikeAdmin(admin.ModelAdmin):
     list_filter=["user__username","blog__title"]
     search_fields=["user__username","blog__title"]
 
+    def save_model(self, request, obj, form, change):
+        if not change:
+            if Like.objects.filter(user=obj.user,blog=obj.blog).exists():
+                self.message_user(request,"This user already liked this blog.", level='ERROR')
+                return
+            obj.save()
+            obj.blog.like_count += 1
+            obj.blog.save()
+        else:
+            obj.save()
 
 admin.site.register(Like,LikeAdmin)
